1abd8c61b817795bd3242858f8f70318c1187019
Maria Martinkous <marticaida@gmail.com>
2022-09-15 02:05:05 -0500

    basic calculation works

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // number keys
-const keys = document.querySelectorAll('.keys');
+const digits = document.querySelectorAll('.digits');
 const keyOne = document.getElementById('one').value = 1;
 const keyTwo = document.getElementById('two').value = 2;
 const keyThree = document.getElementById('three').value = 3;
@@ -12,7 +12,8 @@ const keyNine = document.getElementById('nine').value = 9;
 const keyZero = document.getElementById('zero');
 // possibly need to separate value to add event listener
 keyZero.value = 0;
-const keyPosNeg = document.getElementById('posneg').value = '-';
+const keyPosNeg = document.getElementById('posneg');
+keyPosNeg.value = '-';
 const keyDecimal = document.getElementById('decimal');
 keyDecimal.value = '.';
 // operator and clear keys
@@ -26,74 +27,87 @@ const backspace = document.getElementById('backspace');
 const keyEqual = document.getElementById('equal');
 const display = document.getElementById('display');
 let displayValue;
-let displayContent = display.textContent;
 
 //default display
 display.textContent = 0;
 
+let integers = [];
+var numbers = [];
+var num;
 let num1;
 let num2;
+let operator;
+let result;
 
-keys.forEach((key) => {
-  key.addEventListener('click', () => {
-    const val = key.value;
+//!!!!!!!!!! to store the result of a completed operation for later use
+// result = operate(num1, operator, num2);
+// displayValue = result;
+// num1 = result;
+// display.textContent = displayValue;
+
+digits.forEach((digit) => {
+  digit.addEventListener('click', () => {
+    const val = digit.value;
     if (display.textContent == 0) {
       if (val == 0) {
         display.textContent = 0;
-        console.log(display.textContent);
+        integers.push(display.textContent);
       } else if (val == '.') {
         display.textContent += val;
+        integers.push(display.textContent);
       } else if (display.textContent.includes('.') && !val == 0) {
         display.textContent += val;
-        console.log(display.textContent);
+        integers.push(display.textContent);
       } else if (!val == 0) {
         display.textContent = '';
         display.textContent += val;
-        console.log(display.textContent);
+        integers.push(display.textContent);
       }
     } else if (!display.textContent == 0) {
-      display.textContent += val;
-      console.log(display.textContent);
+      if (display.textContent == sign) {
+        display.textContent = '';
+        display.textContent += val;
+        integers.push(display.textContent);
+      } else
+        display.textContent += val;
+      displayValue = display.textContent;
+      integers.push(displayValue);
     };
+    num = parseFloat(integers[integers.length - 1]);
   });
 });
+
+function toNumbers(num) {
+  numbers.push(num);
+}
 
 keyDecimal.addEventListener('click', () => {
   const val = keyDecimal.value;
   if (!display.textContent.includes('.')) {
     display.textContent += val;
-    console.log(display.textContent);
-
-  }
-
+  };
 });
 
-//object? with two numbers
+// do something with 0 and negative? 
+keyPosNeg.addEventListener('click', () => {
+  const val = keyPosNeg.value;
+  if (!display.textContent.includes('-')) {
+    display.textContent = val + display.textContent;
+  }
+  // else if (display.textContent.includes('-')) {
+  //  // how to reverse negative number to positive?  
+  //   }
+});
 
-// let num2 = 0;
-// num2 = keys.forEach((key) => {
-//   key.addEventListener('click', () => {
-//     const val = key.value;
-// //     if (displayContent = '') {
-// // //do something about 0 to display it only once
-// //     }
-//     return num2 = display.textContent += val;
-//   });
-// });
-
-let sign = "";
+var sign = [];
 operators.forEach((operator) => {
   operator.addEventListener('click', () => {
-    const val = operator.value;
-    sign = val;
-    display.textContent = sign;
+    toNumbers(num);
+    display.textContent = operator.value;
+    num = '';
+    sign.push(operator.value);
   });
-  return sign;
 });
-
-function displayOperation() {
-  display.textContent;
-}
 
 backspace.addEventListener('click', () => {
   let temp = display.textContent;
@@ -109,41 +123,27 @@ keyClear.addEventListener('click', () => {
   display.textContent = 0;
 });
 
+keyEqual.addEventListener('click', () => {
+  toNumbers(num);
+  num1 = numbers[0];
+  num2 = numbers[1];
+  operator = sign[0];
+  operate(num1, operator, num2);
+  console.log(result);
+});
 
-// // pass variables into operate function
-// keyEqual.addEventListener('click', () => {
-//   display.textContent = 
-// });
-
-// math operations
-function add(num1, num2) {
-  return num1 + num2;
-};
-function subtract(num1, num2) {
-  return num1 - num2;
-};
-function multiply(num1, num2) {
-  return num1 * num2;
-};
-function divide(num1, num2) {
-  return num1 / num2;
-};
-
-function operate(operator, num1, num2) {
-  let result;
-  if (operator === "+") {
-    result = add(num1, num2);
-  } else if (operator === "-") {
-    result = subtract(num1, num2);
-  } else if (operator === "*") {
-    result = multiply(num1, num2);
-  } else if (operator === "/") {
-    result = divide(num1, num2);
+function operate(num1, operator, num2) {
+  if (operator == "+") {
+    result = num1 + num2;
+  } else if (operator == "-") {
+    result = num1 - num2;
+  } else if (operator == "*") {
+    result = num1 * num2;
+  } else if (operator == "/") {
+    result = num1 / num2;
   };
-  displayContent = result;
+  display.textContent = result;
 };
-
-
 
 
 // const sum = function (array) {
